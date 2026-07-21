53dfd535e4835ff5681d8505887652a3b50e220c
vickoboy104-hub <support@belovedsubp.com>
2026-07-21 12:31:35 +0100

    ui: include themed public component refinements

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,4 +3,5 @@ import '../css/admin-workspaces.css';
 import '../css/learning-workspaces.css';
 import '../css/finance-workspaces.css';
 import '../css/public-cms.css';
+import '../css/public-cms-components.css';
 import './theme-manager.js';
