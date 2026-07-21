89f0e8e6560f4d23677e67f5c177ebb39d7cf7f8
vickoboy104-hub <support@belovedsubp.com>
2026-07-21 12:09:42 +0100

    feat: add queued public contact notification

---
+++ app/Notifications/ContactMessageReceived.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\ContactMessage;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ContactMessageReceived extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    public function __construct(public readonly ContactMessage $contactMessage) {}
+
+    /**
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('New website enquiry: '.($this->contactMessage->subject ?: 'General enquiry'))
+            ->greeting('A new public website enquiry has been received.')
+            ->line('Name: '.$this->contactMessage->name)
+            ->line('Email: '.($this->contactMessage->email ?: 'Not provided'))
+            ->line('Phone: '.($this->contactMessage->phone ?: 'Not provided'))
+            ->line('Subject: '.($this->contactMessage->subject ?: 'General enquiry'))
+            ->line($this->contactMessage->message)
+            ->line('Open the Website CMS contact inbox to review and update this enquiry.');
+    }
+}
