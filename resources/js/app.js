a64a999eedd8349b8568847121b1e0b3186b98af
vickoboy104-hub <support@belovedsubp.com>
2026-07-21 12:23:49 +0100

    ui: include public website CMS and theme styles

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,4 @@ import '../css/surfaces.css';
 import '../css/admin-workspaces.css';
 import '../css/learning-workspaces.css';
 import '../css/finance-workspaces.css';
+import '../css/public-cms.css';
