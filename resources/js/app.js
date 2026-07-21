05a4b7327e8787b9b5e8c31ff99f8ee550bb017f
vickoboy104-hub <support@belovedsubp.com>
2026-07-21 13:16:41 +0100

    ui: include communication and system administration styles

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,4 +4,5 @@ import '../css/learning-workspaces.css';
 import '../css/finance-workspaces.css';
 import '../css/public-cms.css';
 import '../css/public-cms-components.css';
+import '../css/communication-system.css';
 import './theme-manager.js';
