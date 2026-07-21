ad88a421e7ea9a2aeef33419870317a2492f39af
vickoboy104-hub <support@belovedsubp.com>
2026-07-21 12:30:58 +0100

    ui: enable live theme-token synchronization

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,3 +3,4 @@ import '../css/admin-workspaces.css';
 import '../css/learning-workspaces.css';
 import '../css/finance-workspaces.css';
 import '../css/public-cms.css';
+import './theme-manager.js';
